095c8cc42aaa04eb87924fe88846981b3814746b
jaswanthnarne <2203031260150@paruluniversity.ac.in>
2025-10-17 09:55:12 +0530

    The commit for the integration of the baceknd to the UI

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const API_BASE_URL = 'http://localhost:5000/api';
-const API_BASE_URL = 'https://ctftechdbackend-production.up.railway.app/api';
+const API_BASE_URL = 'https://ctf-tech-d-backend.vercel.app/';
 
 // Create axios instance
 const api = axios.create({
